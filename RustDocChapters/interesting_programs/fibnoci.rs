2c23f799deb1af9d25e2a15c4e1f57d26d12f86b
kamallearner123 <kamalbec2004@gmail.com>
2022-01-18 00:01:59 +0530

    adding fibanocci

---
+++ RustDocChapters/interesting_programs/fibnoci.rs
@@ -0,0 +1,11 @@
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+fn main() {
+    println!("{}",fibonacci(10));
+}
